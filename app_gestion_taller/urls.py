c00e6adb1a0ebda5f7bd77307943a894c72d115c
Gonzaloogm <146828065+Gonzaloogm@users.noreply.github.com>
2026-03-12 13:17:27 +0100

    Practica 5

--- app_gestion_taller/urls.py
+++ app_gestion_taller/urls.py
@@ -2,14 +2,17 @@ from django.urls import path
 from .views import (
     registrar_cliente, registrar_coche, registrar_servicio,
     buscar_cliente, buscar_coche_por_matricula,
-    buscar_coches_de_cliente, buscar_servicios_de_coche
+    buscar_coches_de_cliente, lista_clientes, detalle_cliente, servicios_de_coche
 )
 urlpatterns = [
+    path('lista_clientes/', lista_clientes, name='lista_clientes'),
+    path('detalle_cliente/<int:cliente_id>/', detalle_cliente, name='detalle_cliente'),
+    path('servicios_de_coche/<int:coche_id>/', servicios_de_coche, name='servicios_de_coche'),
     path('clientes/registrar/', registrar_cliente, name='registrar_cliente'),
     path('coches/registrar/', registrar_coche, name='registrar_coche'),
     path('servicios/registrar/', registrar_servicio, name='registrar_servicio'),
     path('clientes/<int:cliente_id>/', buscar_cliente, name='buscar_cliente'),
     path('coches/matricula/<str:matricula>/', buscar_coche_por_matricula, name='buscar_coche_por_matricula'),
     path('clientes/<int:cliente_id>/coches/', buscar_coches_de_cliente, name='buscar_coches_de_cliente'),
-    path('coches/<int:coche_id>/servicios/', buscar_servicios_de_coche, name='buscar_servicios_de_coche'),
+    #path('coches/<int:coche_id>/servicios/', buscar_servicios_de_coche, name='buscar_servicios_de_coche'),
 ]
